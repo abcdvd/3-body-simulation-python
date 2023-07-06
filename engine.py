ae0f630d7544d88371dc40dfbcc1086bbf77c2f3
IMAJNE <87563747+abcdvd@users.noreply.github.com>
2023-07-06 21:59:46 +0900

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,17 @@
+# Constant variable
+G = 6.6743 * 10e-11
+m_s = 1.989 * 10e30
+m_e = 5.972 * 10e24
+r_se = 150 * 1000000 * 1000
+F_std = G * m_s * m_e / (r_se**2)
+
+# input data
+alpha_mass = 1.0788
+alpha_location = [0, 0, 0]
+
+beta_mass = 0.9092
+beta_location = [0.3, 0, 0]
+
+proxima_mass = 0.1221
+proxima_location = [0.1, 0.2, 0.3]
+
